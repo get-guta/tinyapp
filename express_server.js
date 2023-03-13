a9644431dbfa89378e9b2fb479a8cf7c0f87f4da
getenesh <getenesh.guta@gmail.com>
2023-03-13 16:03:09 -0400

    Add header

--- express_server.js
+++ express_server.js
@@ -16,7 +16,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 app.get("/urls/:id", (req, res) => {
-  console.log(req.params.id);
+
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
