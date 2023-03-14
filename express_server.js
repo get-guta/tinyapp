848a3d45fcb96e2286e5305c08b92d430a7db2c1
getenesh <getenesh.guta@gmail.com>
2023-03-13 22:55:30 -0400

    post method

--- express_server.js
+++ express_server.js
@@ -4,7 +4,20 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  let shortUrl = (Math.random() + 1).toString(36).substring(6)
+  console.log(shortUrl);
+
+}
+
+generateRandomString()
 const urlDatabase = {
   "b2xVn2": "http://localhost:8080/urls",
   "9sm5xK": "http://www.google.com",
@@ -15,10 +28,16 @@ app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase}
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
