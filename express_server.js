561636ccddebbdd1ee9cadecce2fa9efda057325
getenesh <getenesh.guta@gmail.com>
2023-03-15 23:46:23 -0400

    Passing cookie through pages

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 
 const express = require("express");
+const cookie_parser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookie_parser());
 const urlDatabase = {
   "b2xVn2": "http://localhost:8080/urls",
   "9sm5xK": "http://www.google.com",
@@ -25,7 +27,10 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -46,8 +51,9 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username).redirect("/urls");
-  
+  res.cookie('username', req.body.username);
+  res.redirect("/urls");
+
 })
 
 
@@ -63,13 +69,20 @@ app.get("/u/:id", (req, res) => {
 });
 // renders availble urls
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  }
   res.render("urls_index", templateVars);
 });
 
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
